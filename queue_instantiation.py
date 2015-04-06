10cc9d43773910f1147ad2023457e9fdbb95509c
Tanner Stirrat <tstirrat@gmail.com>
2015-04-06 09:24:00 -0500

    thao continued updating the queue instantiation file. not sure about finished status, though.

--- queue_instantiation.py
+++ queue_instantiation.py
@@ -14,9 +14,10 @@ def instantiate_queue(seat_list, luggage_time):
 
 
 def random_seat(seat_per_row, num_rows):
-    possible_seats = list(chain([[i] * seats_per_row
+    possible_seats = list(chain(*[[i] * seat_per_row
                           for i in range(1, num_rows + 1)]))
-    return shuffle(possible_seats)
+    shuffle(possible_seats)
+    return possible_seats
 
 
 def back_to_front(num_rows, seat_per_row, num_groups):
@@ -24,12 +25,12 @@ def back_to_front(num_rows, seat_per_row, num_groups):
     Create strategy for Back to Front boarding strategy
     """
     max_rows_per_group = num_rows / num_groups
-    possible_seats = [[[i] * seats_per_row
-                      for i in range(k, min(num_rows + 1, k + max_rows_per_groups))]
-                      for k in range(1, num_rows + 1, max_rows_per_group)]
-    possible_seats = [shuffle(group) for group in possible_seats]
+    possible_seats = [list(range(k, min(num_rows + 1, k + max_rows_per_group))) * seat_per_row \
+                            for k in range(1, num_rows + 1, max_rows_per_group)]
+    for group in possible_seats:
+        shuffle(group)
     possible_seats.reverse()
-    return list(chain(possible_seats))
+    return list(chain(*possible_seats))
 
 
 def front_to_back(num_rows, seat_per_row, num_groups):
@@ -37,10 +38,11 @@ def front_to_back(num_rows, seat_per_row, num_groups):
     Create strategy for Back to Front boarding strategy
     """
     max_rows_per_group = num_rows / num_groups
-    possible_seats = [[[i] * seats_per_row for i in range(k, min(num_rows + 1, k + max_rows_per_groups))]
-                      for k in range(1, num_rows + 1, max_rows_per_group)]
-    possible_seats = [shuffle(group) for group in possible_seats]
-    return list(chain(possible_seats))
+    possible_seats = [list(range(k, min(num_rows + 1, k + max_rows_per_group))) * seat_per_row \
+                            for k in range(1, num_rows + 1, max_rows_per_group)]
+    for group in possible_seats:
+        shuffle(group)
+    return list(chain(*possible_seats))
 
 
 def rotating_zone(num_rows, seat_per_row, num_groups):
@@ -51,24 +53,38 @@ def rotating_zone(num_rows, seat_per_row, num_groups):
     # groups = [[[i] * seats_per_row for i in range(k, k + max_rows_per_groups - 1)] \
     # for k in range(1, num_rows, max_rows_per_group)]
     # group_orders = range(num_groups, 0)
-    unordered_groups = deque([[[i] * seats_per_row for i in range(k, min(num_rows + 1, k + max_rows_per_groups))]
-                             for k in range(1, num_rows + 1, max_rows_per_group)])
-    groups = deque()
+    unordered_groups = deque([list(range(k, min(num_rows + 1, k + max_rows_per_group))) * seat_per_row \
+                            for k in range(1, num_rows + 1, max_rows_per_group)])
+    groups = list()
     while True:
         try:
             groups.append(unordered_groups.pop())
             groups.append(unordered_groups.popleft())
         except IndexError:
             break
-    groups = [shuffle(group) for group in groups]
-    return list(chain(groups))
+    for group in groups:
+        shuffle(group)
+    return list(chain(*groups))
 
 
 def outside_in(num_rows, seat_per_row):
     """
     Board windows - middle - aisle seats
-    Assumption: even num seat / row , 1 aisle -> What about 2?
+    Assumption: even num seat / row . if 2 aisle. Consider creating two separate queue.
     """
-    boarding_group = [[i] * 2 for range(1, num_rows + 1)] for _ in range()
-    boarding_group = 
-    # not finished
+
+    if seat_per_row % 2 == 0:
+        boarding_group = [list(range(1, num_rows + 1)) * 2 for _ in range(seat_per_row / 2)]
+    else:
+        #For plane ERJ145 (3 seats / row)
+        boarding_group = [list(range(1, num_rows + 1)) * 2 for _ in range(seat_per_row / 2)] + [list(range(1, num_rows + 1))]
+    for group in boarding_group:
+        shuffle(group)
+    return list(chain(*boarding_group))
+
+
+if __name__ == '__main__':
+    pass
+
+
+    
